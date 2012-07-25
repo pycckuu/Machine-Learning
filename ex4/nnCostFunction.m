a021869cf01d9746b7f206682f575a40563eefc0
Igor <is.markelov@gmail.com>
2012-07-25 22:19:31 +0400

    Working part 1 "Feedforward and cost function"

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -39,48 +39,7 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
-%X = [ones(m, 1) X];
 
-%X1 = sigmoid(X*Theta1');
-
-%m = size(X1, 1);
-
-%X1 = [ones(m, 1) X1];
-
-%A = sigmoid(X1*Theta2');
-
-%J= ((-y').*log(A)-(1-y').*log(1-A));
-
-%X = [ones(m, 1) X];
-%A_1=[ones(size(X,1),1),X];
-%Z_2=A_1*Theta1';
-%A_2=sigmoid(Z_2);
-%A_2=[ones(size(A_2,1),1),A_2];
-%Z_3=A_2*Theta2';
-%A_3=sigmoid(Z_3);
-%yunroll=zeros(m,num_labels);
-%for i=1:m
-%     yunroll(i,y(i))=1;
-%   endfor;
-%   computeJ =-(yunroll.*log(A_3) - (1-yunroll).*(1-log(A_3)));
-%   J=(1/m)*sum(sum(computeJ,2));
-%z2 = X*(Theta1');
-
-%a2 = sigmoid(z2);
-
-%a2 = [ones(size(a2,1),1) a2];
-%size(a2);
-%z3 = a2*(Theta2');
-
-%a3 = sigmoid(z3);
-
-%Unroll y into a 0 and 1 matrix of 5000x10
-
-%vec_y = eye(num_labels)(y,:);
-
-%Compute J
-
-%J = sum(sum((-vec_y.*log(a3))-((1-vec_y).*log(1-a3))))/m;
 
 a1 = [ones(size(X, 1), 1) X];
 a2 = sigmoid(Theta1 * a1');
@@ -94,20 +53,6 @@ aYV = size(yV) %This is returning 5000x10 like it should
 
 
 J = (1/m)*sum(sum( log(Hx).*(-yV)' - log(1-Hx).*(1-yV)' ));
-
-% Add ones to the X data matrix
-%X = [ones(m, 1) X];
-%a2=sigmoid(X*Theta1');
-%a2 = [ones(m, 1) a2];
-%Hx=sigmoid(a2*Theta2');
-%yV = repmat(1:num_labels,m,1)==repmat(y,1,num_labels);
-%J = (1/m)*sum(sum( log(Hx).*(-yV)' - log(1-Hx).*(1-yV)' ));
-
-
-%for iter=1:size(a3,2)	J(:,iter)=(log(a3(:,iter))*(-y)'-log(1-a3(:,iter))*(1-y)')
-%	J=(-y)'*log(a3)-(1-y)'*log(1-a3)	
-%end
-%J=sum(J)/m
 
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
