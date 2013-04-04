2f41430ef9f907478005059885a94df34e3ea6bc
Igor <is.markelov@gmail.com>
2013-04-04 19:11:16 -0400

    up

---
+++ Recognizing hand-written digits/nn.m
@@ -0,0 +1,69 @@
+%% ====== Machine Learning =================================
+% Implementing logistic regression and neural networks to 
+% recognize hand-written digits.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% Setup the parameters
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+load('data1.mat');
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ================ Part 2: Loading Pameters ================
+% Loading some pre-initialized neural network parameters.
+
+fprintf('\nLoading Saved Neural Network Parameters ...\n')
+
+% Loading the weights into variables Theta1 and Theta2
+load('weights.mat');
+
+%% ================= Part 3: Implement Predict =================
+%  Implementing the function to use the
+%  neural network to predict the labels of the training set. computing
+%  the training set accuracy.
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%  Randomly permute examples
+rp = randperm(m);
+
+for i = 1:m
+    % Display 
+    fprintf('\nDisplaying Example Image\n');
+    displayData(X(rp(i), :));
+
+    pred = predict(Theta1, Theta2, X(rp(i),:));
+    fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    
+    % Pause
+    fprintf('Program paused. Press enter to continue.\n');
+    pause;
+end
+
