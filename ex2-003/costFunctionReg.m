61bff04378991da76dc5bb80a57d9f27a89dbf19
pycckuu <is.markelov@gmail.com>
2013-05-15 14:08:44 -0400

    Grad descent

--- ex2-003/costFunctionReg.m
+++ ex2-003/costFunctionReg.m
@@ -11,17 +11,15 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+
+J = (sum( -y .* log(sigmoid(X*theta)) - (1 - y) .* log(1-sigmoid(X*theta)) ) / m ) + lambda/(2 * m) * sum (theta.^2)
+
+grad(1) = sum((sigmoid(X*theta) - y).*X(:,1)) / m;
 
 
+for i=2:size(theta)
+	grad(i) = ( sum((sigmoid(X*theta) - y).*X(:,i)) / m ) + lambda/m * theta(i);
+end
 
-
-
-
-% =============================================================
 
 end
